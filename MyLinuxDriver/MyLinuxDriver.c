b5f51547a93602c955389c93d7ae57acca513ee6
root <root@localhost.localdomain>
2017-07-17 18:13:31 -0400

    Add my linux driver

---
+++ MyLinuxDriver/MyLinuxDriver.c
@@ -0,0 +1,21 @@
+
+/*
+ *  hello-1.c - The simplest kernel module.
+ */
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+
+int init_module(void)
+{
+	printk(KERN_INFO "Installed Linux kernel driver in the call init_module\n");
+
+	/*
+	 * A non 0 return means init_module failed; module can't be loaded.
+	 */
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Uninstalled Linux kernel driver in the call cleanup_module\n");
+}
